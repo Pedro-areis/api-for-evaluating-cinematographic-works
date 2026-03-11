8de0beef347a6356695dd2d1398a4ee18bd0311c
Pedro-areis <pedrogiarola1@gmail.com>
2026-03-11 16:56:53 -0300

    First commit

---
+++ src/test/java/com/example/api_ecw/ApiEcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_ecw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
