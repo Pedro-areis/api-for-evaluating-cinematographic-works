1db48f4950045c8373867e644f7464f9f504f80f
Pedro-areis <pedrogiarola1@gmail.com>
2026-03-16 19:44:19 -0300

    feat(entitys): cria a entidade usuário.

---
+++ src/main/java/com/example/api_ecw/User/User.java
@@ -0,0 +1,38 @@
+package com.example.api_ecw.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = 200)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "date_birth", nullable = false)
+    private LocalDate dateBirth;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
