a6fd110672fcb6070a5f2d92613135384dbaadc6
Pedro-areis <pedrogiarola1@gmail.com>
2026-03-17 20:44:30 -0300

    docs(CRUD): adiciona comentários aos DTOs.

--- src/main/java/com/example/api_ecw/user/dto/UserRequest.java
+++ src/main/java/com/example/api_ecw/user/dto/UserRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+// DTO for User request
 public record UserRequest(
         @NotBlank(message = "Name is required")
         @Size(max= 255, message = "Name must be less than 255 characters")
--- src/main/java/com/example/api_ecw/user/dto/UserResponse.java
+++ src/main/java/com/example/api_ecw/user/dto/UserResponse.java
@@ -3,6 +3,7 @@ package com.example.api_ecw.user.dto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+// DTO for User response
 public record UserResponse(
         String name,
         String email,
