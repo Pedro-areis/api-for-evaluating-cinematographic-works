ab446d4440d46ceb94e190cd635b08f1b8fa56fb
Pedro-areis <pedrogiarola1@gmail.com>
2026-03-19 21:51:09 -0300

    feat(CRUD): cria a classe AuthService para validar que o usuário existe no banco.

---
+++ src/main/java/com/example/api_ecw/user/AuthService.java
@@ -0,0 +1,18 @@
+package com.example.api_ecw.user;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
