ac626ebb8b50c76327a43a8256fc6c2884390d40
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-07 20:49:33 -0300

    test(CRUD-tests): cria as classes updateUser e deleteUser para os testes unitários dos metodos na atualização e exclusão dos usuários.

--- src/test/java/com/example/api_ecw/user/UserServiceTest.java
+++ src/test/java/com/example/api_ecw/user/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.api_ecw.user;
 
 import com.example.api_ecw.user.dto.UserRequest;
+import com.example.api_ecw.user.dto.UserUpdate;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,10 +15,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -82,6 +84,154 @@ class UserServiceTest {
             assertThrows(DataIntegrityViolationException.class, () ->
                     userService.createUser(input));
             verify(userRepository, never()).save(any());
+        }
+    }
+
+    @Nested
+    class updateUser {
+        @Test
+        @DisplayName("Should update user with success when all fields are provided")
+        void shouldUpdateUserWithSuccessWhenAllFieldsAreProvided() {
+            // Arrange
+            User user = new User(
+                    UUID.randomUUID(),
+                    "teste",
+                    "teste@gmail.com",
+                    "123456",
+                    LocalDate.parse("1990-01-01"),
+                    null
+            );
+            when(userRepository.findById(user.getId()))
+                    .thenReturn(Optional.of(user));
+
+            doReturn(user).when(userRepository).save(any());
+
+            var input = new UserUpdate(
+                    "name updated",
+                    "teste_updated@gmail.com",
+                    "12345678",
+                    LocalDate.parse("1990-01-01")
+            );
+            doReturn("12345678").when(passwordEncoder).encode("12345678");
+
+            // Act
+            var result = userService.updateUser(user.getId(), input);
+
+            // Assert
+            assertEquals("name updated", result.name());
+            assertEquals("teste_updated@gmail.com", result.email());
+            assertEquals(LocalDate.parse("1990-01-01"), result.dateBirth());
+        }
+
+        @Test
+        @DisplayName("Should update user with success when some fields are provided")
+        void shouldUpdateUserWithSuccessWhenSomeFieldsAreProvided() {
+            // Arrange
+            User user = new User(
+                    UUID.randomUUID(),
+                    "teste",
+                    "teste@gmail.com",
+                    "123456",
+                    LocalDate.parse("1990-01-01"),
+                    null
+            );
+            when(userRepository.findById(user.getId()))
+                    .thenReturn(Optional.of(user));
+
+            doReturn(user).when(userRepository).save(any());
+
+            var input = new UserUpdate(
+                    null,
+                    "teste_updated@gmail.com",
+                    "12345678",
+                    null
+            );
+            doReturn("12345678").when(passwordEncoder).encode("12345678");
+
+            // Act
+            var result = userService.updateUser(user.getId(), input);
+
+            // Assert
+            assertEquals("teste_updated@gmail.com", result.email());
+        }
+
+        @Test
+        @DisplayName("Should return exception when user not found")
+        void shouldReturnExceptionWhenUserNotFound() {
+            // Arrange
+            var input = new UserUpdate(
+                    "name",
+                    "teste@gmail.com",
+                    "123456",
+                    LocalDate.parse("1990-01-01")
+            );
+
+            UUID id = UUID.randomUUID();
+            when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+            // Act & Assert
+            assertThrows(EntityNotFoundException.class,
+                    () -> userService.updateUser(id, input));
+            verify(userRepository, never()).save(any());
+        }
+
+        @Test
+        @DisplayName("Should return exception when email already exists")
+        void shouldReturnExceptionWhenEmailAlreadyExists() {
+            // Arrange
+            User user = new User(
+                    UUID.randomUUID(),
+                    "teste",
+                    "oldEmail@gmail.com",
+                    "123456",
+                    LocalDate.parse("1990-01-01"),
+                    null
+            );
+            when(userRepository.findById(user.getId()))
+                    .thenReturn(Optional.of(user));
+
+            var input = new UserUpdate(
+                    null,
+                    "teste@gmail.com",
+                    null,
+                    null
+            );
+            when(userRepository.findByEmail("teste@gmail.com"))
+                    .thenReturn(Optional.of(new User()));
+
+            // Act & Assert
+            assertThrows(DataIntegrityViolationException.class,
+                    () -> userService.updateUser(user.getId(), input));
+            verify(userRepository, never()).save(any());
+        }
+    }
+
+    @Nested
+    class deleteUser {
+        @Test
+        @DisplayName("Should delete user with success")
+        void shouldDeleteUserWithSuccess() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            when(userRepository.findById(id))
+                    .thenReturn(Optional.of(new User()));
+
+            // Act & Assert
+            userService.deleteUser(id);
+        }
+
+        @Test
+        @DisplayName("Should return exception when user not found")
+        void shouldReturnExceptionWhenUserNotFound() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            when(userRepository.findById(id))
+                    .thenReturn(Optional.empty());
+
+            // Act & Assert
+            assertThrows(RuntimeException.class,
+                    () -> userService.deleteUser(id));
+            verify(userRepository, never()).delete(any());
         }
     }
 
