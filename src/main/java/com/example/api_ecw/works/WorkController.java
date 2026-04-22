972abbde051eab590606dc67ba350dc4e3e4d422
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 16:23:50 -0300

    feat(works): cria endpoint para adicionar uma nova obra e adiciona o tipo "ResponseEntity" para retorno dos status.

--- src/main/java/com/example/api_ecw/works/WorkController.java
+++ src/main/java/com/example/api_ecw/works/WorkController.java
@@ -1,8 +1,10 @@
 package com.example.api_ecw.works;
 
 import com.example.api_ecw.works.dto.TmdbSearchResponse;
-import com.example.api_ecw.works.dto.TmdbWorkResponse;
+import com.example.api_ecw.works.dto.WorkResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,9 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class WorkController {
     private final TmdbIntegrationService tmdbIntegrationService;
+    private final WorkService workService;
 
     @GetMapping("/search/{title}")
-    public TmdbSearchResponse getWorkByTitle(@PathVariable String title) {
-        return tmdbIntegrationService.getWorkByTitle(title);
+    public ResponseEntity<TmdbSearchResponse> getWorkByTitle(@PathVariable String title) {
+        TmdbSearchResponse response = tmdbIntegrationService.getWorkByTitle(title);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<WorkResponse> createWork(@RequestBody Work work) {
+        WorkResponse response = workService.create(work);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
