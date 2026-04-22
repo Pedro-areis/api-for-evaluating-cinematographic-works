b93be2025851d5de4b95c9d9f6c9099746df25c2
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 17:30:33 -0300

    feat(works): cria a DTO WorkRequest.

--- src/main/java/com/example/api_ecw/works/dto/WorkRequest.java
+++ src/main/java/com/example/api_ecw/works/dto/WorkRequest.java
@@ -1,4 +1,30 @@
 package com.example.api_ecw.works.dto;
 
-public record WorkRequest() {
+import com.example.api_ecw.enums.WorkType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record WorkRequest(
+        @NotBlank(message = "Title is required")
+        String title,
+
+        @NotBlank(message = "Synopsis is required")
+        String synopsis,
+
+        @NotNull(message = "Type is required")
+        WorkType type,
+
+        @NotNull(message = "Genre IDs is required")
+        List<Integer> genreIds,
+
+        @NotNull(message = "TMDB ID is required")
+        Integer tmdbId,
+
+        @NotNull(message = "Release date is required")
+        LocalDate releaseDate
+) {
 }
