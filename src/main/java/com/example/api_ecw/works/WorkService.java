9271ee1d2f5a7f8adc9cf92fc20a1e3c2d0f6f5d
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 16:21:41 -0300

    feat(works): cria o service para a entidade work. cria o metodo create para criar um novo dado no banco de dados.

--- src/main/java/com/example/api_ecw/works/WorkService.java
+++ src/main/java/com/example/api_ecw/works/WorkService.java
@@ -1,4 +1,25 @@
 package com.example.api_ecw.works;
 
+import com.example.api_ecw.works.dto.WorkResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class WorkService {
+    private final WorkRepository workRepository;
+
+    public WorkResponse create(Work work) {
+        Work newWork = workRepository.save(work);
+
+        return new WorkResponse(
+                newWork.getId(),
+                newWork.getTitle(),
+                newWork.getSynopsis(),
+                newWork.getScore(),
+                newWork.getType(),
+                newWork.getReleaseDate(),
+                newWork.getCreatedAt()
+        );
+    }
 }
