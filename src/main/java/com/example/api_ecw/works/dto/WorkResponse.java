fd5d1f2374fc6fb244ff598e9cf97db4acee3d15
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 16:22:22 -0300

    feat(works): cria DTO para retornar os dados da obra em questão.

--- src/main/java/com/example/api_ecw/works/dto/WorkResponse.java
+++ src/main/java/com/example/api_ecw/works/dto/WorkResponse.java
@@ -1,4 +1,17 @@
 package com.example.api_ecw.works.dto;
 
-public record WorkResponse() {
-}
+import com.example.api_ecw.enums.WorkType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record WorkResponse(
+        UUID id,
+        String tilte,
+        String synopsis,
+        Float score,
+        WorkType type,
+        LocalDate releaseDate,
+        LocalDateTime createdAt
+) {}
