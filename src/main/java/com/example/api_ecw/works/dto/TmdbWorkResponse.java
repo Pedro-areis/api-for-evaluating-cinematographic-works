c7aa3c748d26f4750da9df927d1f7e4619b8cf5f
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 16:25:45 -0300

    feat(works): adiciona a anotação JsonAlias para buscar campos das series.

--- src/main/java/com/example/api_ecw/works/dto/TmdbWorkResponse.java
+++ src/main/java/com/example/api_ecw/works/dto/TmdbWorkResponse.java
@@ -1,5 +1,6 @@
 package com.example.api_ecw.works.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.lang.reflect.Array;
@@ -7,10 +8,11 @@ import java.util.List;
 
 public record TmdbWorkResponse(
         Long id,
-        @JsonProperty("original_title") String originalTitle,
-        String title,
+        @JsonProperty("original_title") @JsonAlias("original_name") String originalTitle,
+        @JsonProperty("title") @JsonAlias("name") String title,
         @JsonProperty("original_language") String originalLanguage,
-        @JsonProperty("release_date") String releaseDate,
+        @JsonProperty("release_date") @JsonAlias("first_air_date") String releaseDate,
         String overview,
+        @JsonProperty("media_type") String mediaType,
         @JsonProperty("genre_ids") List<Integer> genreIds
 ) {}
