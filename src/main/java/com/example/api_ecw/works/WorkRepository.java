9a9c29d633da365898316bcf57af121b1ee108fb
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 16:22:52 -0300

    feat(works): cria metodo para buscar uma obra pelo seu ID.

--- src/main/java/com/example/api_ecw/works/WorkRepository.java
+++ src/main/java/com/example/api_ecw/works/WorkRepository.java
@@ -2,7 +2,9 @@ package com.example.api_ecw.works;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WorkRepository extends JpaRepository<Work, UUID> {
+    Optional<Work> findById(UUID id);
 }
