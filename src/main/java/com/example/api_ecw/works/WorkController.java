90d481e817390c6d42fd8a74283953810610c48c
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 15:21:20 -0300

    feat(works): cria classe WorkController, cria um endpoint para buscar uma obra pelo nome.

--- src/main/java/com/example/api_ecw/works/WorkController.java
+++ src/main/java/com/example/api_ecw/works/WorkController.java
@@ -1,4 +1,18 @@
 package com.example.api_ecw.works;
 
+import com.example.api_ecw.works.dto.TmdbSearchResponse;
+import com.example.api_ecw.works.dto.TmdbWorkResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/works")
+@RequiredArgsConstructor
 public class WorkController {
+    private final TmdbIntegrationService tmdbIntegrationService;
+
+    @GetMapping("/search/{title}")
+    public TmdbSearchResponse getWorkByTitle(@PathVariable String title) {
+        return tmdbIntegrationService.getWorkByTitle(title);
+    }
 }
