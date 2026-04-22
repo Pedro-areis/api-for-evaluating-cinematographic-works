6749c47c7312ea5da6f35b0e116d1e4b5803f6b0
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-22 15:22:31 -0300

    feat(works): cria classe service para integração da API externa. Cria metodo para buscar a obra pelo nome.

---
+++ src/main/java/com/example/api_ecw/works/TmdbIntegrationService.java
@@ -0,0 +1,31 @@
+package com.example.api_ecw.works;
+
+import com.example.api_ecw.works.dto.TmdbSearchResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+
+public class TmdbIntegrationService {
+    private final RestClient restClient;
+    private final String apiKey;
+
+    public TmdbIntegrationService (
+            @Value("${tmdb.api.url}") String apiUrl,
+            @Value("${tmdb.api.key}") String apiKey) {
+
+        this.apiKey = apiKey;
+        restClient = RestClient.builder()
+                .baseUrl(apiUrl)
+                .defaultHeader("accept", "application/json")
+                .build();
+    }
+
+    public TmdbSearchResponse getWorkByTitle(String title) {
+        return restClient.get()
+                .uri("/search/movie?query={title}&include_adult=false&language=pt-BR&page=1&api_key={apiKey}", title, apiKey)
+                .retrieve()
+                .body(TmdbSearchResponse.class);
+    }
+}
