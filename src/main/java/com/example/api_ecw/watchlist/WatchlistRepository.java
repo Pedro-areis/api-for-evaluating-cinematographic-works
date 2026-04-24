b4ec26aad52506acfa74241830f8c4337f73911e
Pedro-areis <pedrogiarola1@gmail.com>
2026-04-24 12:15:17 -0300

    feat(works): cria a WatchlistRepository e o metodo para validar se a obra já existe na Watchlist do usuário.

---
+++ src/main/java/com/example/api_ecw/watchlist/WatchlistRepository.java
@@ -0,0 +1,12 @@
+package com.example.api_ecw.watchlist;
+
+import com.example.api_ecw.user.User;
+import com.example.api_ecw.works.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface WatchlistRepository extends JpaRepository<Watchlist, UUID> {
+    boolean existsByUserAndWork(User user, Work work);
+}
