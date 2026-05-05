43ba674ad8b024f8ef3b6bc1fb4efc97465ad54b
Pedro-areis <pedrogiarola1@gmail.com>
2026-05-05 12:43:22 -0300

    feat(posts/scores): adiciona o metodo "findByTmdbIdAndType" para buscar a buscar obras separadamente, filme ou série.

--- src/main/java/com/example/api_ecw/works/WorkRepository.java
+++ src/main/java/com/example/api_ecw/works/WorkRepository.java
@@ -1,10 +1,12 @@
 package com.example.api_ecw.works;
 
+import com.example.api_ecw.enums.WorkType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface WorkRepository extends JpaRepository<Work, UUID> {
+    Optional<Work> findByTmdbIdAndType(Integer tmdbId, WorkType type);
     Optional<Work> findByTmdbId(Integer tmdbId);
 }
