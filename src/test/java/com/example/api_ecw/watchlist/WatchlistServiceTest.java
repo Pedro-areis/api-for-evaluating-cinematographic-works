40b4ca2cc772b0ece4658584481507d4d10618d3
Pedro-areis <pedrogiarola1@gmail.com>
2026-05-11 23:23:54 -0300

    test(test-watchlist): cria metodo para validar que, caso a obra não exista na base de dados, seja buscado na API externa; valida que será retornado erro ao tentar criar uma watchlist sem um user; adiciona uma instância para a variável "work" com BeforeEach.

--- src/test/java/com/example/api_ecw/watchlist/WatchlistServiceTest.java
+++ src/test/java/com/example/api_ecw/watchlist/WatchlistServiceTest.java
@@ -4,10 +4,14 @@ import com.example.api_ecw.enums.WorkStatus;
 import com.example.api_ecw.enums.WorkType;
 import com.example.api_ecw.scores.ScoreRepository;
 import com.example.api_ecw.tmdb_api.TmdbIntegrationService;
+import com.example.api_ecw.tmdb_api.dto.TmdbGenre;
+import com.example.api_ecw.tmdb_api.dto.TmdbMovieResponse;
 import com.example.api_ecw.user.User;
 import com.example.api_ecw.user.UserRepository;
 import com.example.api_ecw.works.Work;
 import com.example.api_ecw.works.WorkRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,12 +24,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -47,6 +48,23 @@ class WatchlistServiceTest {
     @InjectMocks
     private WatchlistService watchlistService;
 
+    private Work work;
+
+    @BeforeEach
+    void setUp() {
+        work = new Work(
+                UUID.randomUUID(),
+                "movie",
+                "synopsis",
+                10.0f,
+                WorkType.movie,
+                LocalDate.parse("2023-01-01"),
+                LocalDateTime.now(),
+                List.of(1,2,3),
+                1
+        );
+    }
+
     @Nested
     class addMovieToWatchlist {
         @Test
@@ -55,18 +73,6 @@ class WatchlistServiceTest {
             // Arrange
             Integer tmdbId = 1;
             UUID userId = UUID.randomUUID();
-
-            Work work = new Work(
-                    UUID.randomUUID(),
-                    "movie",
-                    "synopsis",
-                    10.0f,
-                    WorkType.movie,
-                    LocalDate.parse("2023-01-01"),
-                    LocalDateTime.now(),
-                    List.of(1,2,3),
-                    tmdbId
-            );
 
             User user = new User();
             user.setId(userId);
@@ -92,6 +98,76 @@ class WatchlistServiceTest {
             assertEquals(user, saved.getUser());
 
             assertNotNull(result);
+        }
+
+        @Test
+        @DisplayName("Should add movie to database when not exist")
+        void shouldAddMovieToDataBaseWhenNotExist() {
+            // Arrange
+            Integer tmdbId = 1;
+            UUID userId = UUID.randomUUID();
+
+            TmdbMovieResponse response = new TmdbMovieResponse(
+                    1,
+                    "originalTitle",
+                    "title",
+                    "pt-BR",
+                    "1999-01-01",
+                    "overview",
+                    List.of(
+                            new TmdbGenre(1, "test")
+                    )
+            );
+
+            User user = new User();
+            user.setId(userId);
+
+            when(workRepository.findByTmdbId(tmdbId)).thenReturn(Optional.empty());
+            when(tmdbIntegrationService.getMovieByTmdbId(tmdbId)).thenReturn(response);
+
+            when(workRepository.save(any(Work.class))).thenAnswer(i -> i.getArgument(0));
+
+            when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+            when(watchlistRepository.existsByUserAndWorkAndType(
+                    eq(user),
+                    any(Work.class),
+                    eq(WorkType.movie)
+            )).thenReturn(false);
+
+            when(watchlistRepository.save(any(Watchlist.class))).thenAnswer(i -> i.getArgument(0));
+
+            // Act
+            var result = watchlistService.addMovieToWatchlist(userId, tmdbId);
+
+            // Assert
+            verify(watchlistRepository).save(watchlistCaptor.capture());
+
+            Watchlist saved = watchlistCaptor.getValue();
+
+            assertEquals("title", saved.getName());
+            assertEquals(WorkStatus.pending, saved.getStatus());
+            assertEquals(WorkType.movie, saved.getType());
+            assertEquals(user, saved.getUser());
+
+            assertNotNull(saved.getWork());
+            assertNotNull(result);
+        }
+
+        @Test
+        @DisplayName("Should throw exception when user not found")
+        void shouldExceptionWhenUserNotFound() {
+            // Arrange
+            Integer tmdbId = 1;
+            UUID userId = UUID.randomUUID();
+
+            when(workRepository.findByTmdbId(tmdbId)).thenReturn(Optional.of(work));
+            when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+            // Act & Assert
+            assertThrows(EntityNotFoundException.class,
+                    () -> watchlistService.addMovieToWatchlist(userId, tmdbId));
+
+            verify(watchlistRepository, never()).save(any());
         }
     }
 }
