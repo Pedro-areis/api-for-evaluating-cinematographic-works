c918ee7c7bd86a355899ca38fb5f8916a65398c3
Pedro-areis <pedrogiarola1@gmail.com>
2026-05-27 21:00:06 -0300

    feat(github-actions): mocka a classe GenreCacheService na classe main dos testes.

--- src/test/java/com/example/api_ecw/ApiEcwApplicationTests.java
+++ src/test/java/com/example/api_ecw/ApiEcwApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.api_ecw;
 
+import com.example.api_ecw.tmdb_api.GenreCacheService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApiEcwApplicationTests {
+	@MockitoBean
+	private GenreCacheService genreCacheService;
 
 	@Test
 	void contextLoads() {
