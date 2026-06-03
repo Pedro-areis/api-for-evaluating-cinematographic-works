b4ec842471bc5a36941ae2f0196ee31aad446c5d
Pedro-areis <pedrogiarola1@gmail.com>
2026-06-02 21:31:33 -0300

    docs(swagger): documentação personalizada para a DTO UserRequest

--- src/main/java/com/example/api_ecw/user/dto/UserRequest.java
+++ src/main/java/com/example/api_ecw/user/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.api_ecw.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,18 +9,22 @@ import java.time.LocalDate;
 
 // DTO for User request
 public record UserRequest(
+        @Schema(description = "Nome completo do usuário", example = "Pedro Augusto Reis")
         @NotBlank(message = "Name is required")
         @Size(max= 255, message = "Name must be less than 255 characters")
         String name,
 
+        @Schema(description = "E-mail do usuário para login", example = "example@gmail.com")
         @NotBlank(message = "Email is required")
         @Size(max= 255, message = "Email must be less than 255 characters")
         String email,
 
+        @Schema(description = "Senha para login no site", example = "12345678")
         @NotBlank(message = "Password is required")
         @Size(max= 255, message = "Password must be less than 255 characters")
         String password,
 
+        @Schema(description = "Data de nascimento do usuário", example = "2004-08-12")
         @NotNull(message = "Date of birth is required")
         LocalDate dateBirth
 ) {}
